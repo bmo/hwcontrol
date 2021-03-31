37df9b130d13e9fdb70a91ba51d095217d789d84
Brian Moran <brianmoran@gmail.com>
2021-03-31 11:20:42 -0700

    don't store DEBUG files

--- usbser1/Debug/USB_DEVICE/App/EvtQueue.d
+++
@@ -1,9 +0,0 @@
-USB_DEVICE/App/EvtQueue.o: ../USB_DEVICE/App/EvtQueue.c \
- ../USB_DEVICE/App/EvtQueue.h ../USB_DEVICE/App/myevents.h \
- ../USB_DEVICE/App/UsrTimer.h
-
-../USB_DEVICE/App/EvtQueue.h:
-
-../USB_DEVICE/App/myevents.h:
-
-../USB_DEVICE/App/UsrTimer.h:
--- usbser1/Debug/USB_DEVICE/App/UsrTimer.d
+++
@@ -1,4 +0,0 @@
-USB_DEVICE/App/UsrTimer.o: ../USB_DEVICE/App/UsrTimer.c \
- ../USB_DEVICE/App/UsrTimer.h
-
-../USB_DEVICE/App/UsrTimer.h:
